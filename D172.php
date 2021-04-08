ffef169aff54c03431be9fee06967218d2d11818
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-08 16:53:48 +0900

    refactor: 変数の処理を分割し、わかりやすいように変更

--- D172.php
+++ D172.php
@@ -1,6 +1,7 @@
 <?php
     // 自分の得意な言語で
     // Let's チャレンジ！！
-    $InputLine = fgets(STDIN) -1;
-    echo "$InputLine";
+    $inputLine = fgets(STDIN);
+    $dayBefore = $inputLine - 1;
+    echo "$dayBefore";
 ?>
