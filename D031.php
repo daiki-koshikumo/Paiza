1498e61559f85a8229d5d1685251d58ce6533a02
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-08 17:07:08 +0900

    refactor: 変数の処理を分割し、わかりやすいように変更

--- D031.php
+++ D031.php
@@ -1,6 +1,7 @@
 <?php
     // 自分の得意な言語で
     // Let's チャレンジ！！
-    $inputLine = fgets(STDIN) * 60;
-    echo "$inputLine";
+    $inputLine = fgets(STDIN);
+    $MinutesToSeconds = $inputLine * 60;
+    echo "$MinutesToSeconds";
 ?>
