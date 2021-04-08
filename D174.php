b619853553cfefd3b57e6a8d3f40dc0a30d85281
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-08 15:33:50 +0900

    refactor: -変数名をキャメルケースに変更

--- D174.php
+++ D174.php
@@ -1,7 +1,7 @@
 <?php
     // 自分の得意な言語で
     // Let's チャレンジ！！
-    $InputLine = (int)fgets(STDIN);
-    $CalculationResult = $InputLine * 1500;
-    echo "$CalculationResult";
+    $inputLine = (int)fgets(STDIN);
+    $calculationResult = $inputLine * 1500;
+    echo "$calculationResult";
 ?>
