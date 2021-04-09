8497a5327582afc1cd514107c77bf7e02f930f4e
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-09 12:42:14 +0900

    revert: a35fd6c変数名をキャメルケースに変更の修正の修正

--- D172.php
+++ D172.php
@@ -1,12 +1,7 @@
 <?php
     // 自分の得意な言語で
     // Let's チャレンジ！！
-<<<<<<< HEAD
     $inputLine = fgets(STDIN);
     $dayBefore = $inputLine - 1;
     echo $dayBefore;
-=======
-    $inputLine = fgets(STDIN) -1;
-    echo "$inputLine";
->>>>>>> parent of a35fd6c... refactor: 変数名をキャメルケースに変更
 ?>
