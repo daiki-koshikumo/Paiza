c2dd2fdc88fb1e2b7e10e247576314bbce5883b4
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-09 12:39:11 +0900

    refactor:変数名をキャメルケースに変更

--- C086.php
+++ C086.php
@@ -1,7 +1,7 @@
 <?php
     // 自分の得意な言語で
     // Let's チャレンジ！！
-    $input_line = fgets(STDIN);
-    $Strtr = strtr($input_line, array('a' => '', 'e' => '', 'i' => '', 'o' => '', 'u' => '', 'A' => '', 'E' => '', 'I' => '', 'O' => '', 'U' => ''));
-    echo "$Strtr";
+    $inputLine = fgets(STDIN);
+    $strtr = strtr($inputLine, array('a' => '', 'e' => '', 'i' => '', 'o' => '', 'u' => '', 'A' => '', 'E' => '', 'I' => '', 'O' => '', 'U' => ''));
+    echo "$strtr";
 ?>
