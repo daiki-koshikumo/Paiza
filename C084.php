b2875f5210dea825b82c1d0e24c3c77c9dc32ae3
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-09 12:39:20 +0900

    refactor: 変数名をキャメルケースに変更

--- C084.php
+++ C084.php
@@ -1,11 +1,11 @@
 <?php
     // 自分の得意な言語で
     // Let's チャレンジ！！
-    $input_line = fgets(STDIN);
-    $input_line = str_replace(PHP_EOL, '', $input_line);
-    $countInput_line = mb_strlen($input_line);
-    $decoration = str_repeat("+", $countInput_line + 2);
+    $inputLine = fgets(STDIN);
+    $inputLine = str_replace(PHP_EOL, '', $inputLine);
+    $countInputLine = mb_strlen($inputLine);
+    $decoration = str_repeat("+", $countInputLine + 2);
     echo "$decoration\n";
-    echo "+"."$input_line"."+"."\n";
+    echo "+"."$inputLine"."+"."\n";
     echo "$decoration";
 ?>
