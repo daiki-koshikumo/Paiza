1f3f5e57cea68e7fb739d1640d7397d539e44a3c
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-15 14:54:16 +0900

    refactor: 変数名をわかりやすいものに変更

--- C086.php
+++ C086.php
@@ -2,6 +2,6 @@
     // 自分の得意な言語で
     // Let's チャレンジ！！
     $inputLine = fgets(STDIN);
-    $strtr = strtr($inputLine, array('a' => '', 'e' => '', 'i' => '', 'o' => '', 'u' => '', 'A' => '', 'E' => '', 'I' => '', 'O' => '', 'U' => ''));
-    echo $strtr;
+    $vowelRemove = strtr($inputLine, array('a' => '', 'e' => '', 'i' => '', 'o' => '', 'u' => '', 'A' => '', 'E' => '', 'I' => '', 'O' => '', 'U' => ''));
+    echo $vowelRemove;
 ?>
