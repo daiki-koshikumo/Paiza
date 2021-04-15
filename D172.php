d4bad58ae262fbde408a76f37e8f855bb3dc0e9f
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-15 14:09:44 +0900

    Revert "Revert "refactor: 変数名をキャメルケースに変更""
    This reverts commit ad53213d242f6847fe799ff95eac8dda1f57e5ab.

--- D172.php
+++ D172.php
@@ -1,12 +1,7 @@
 <?php
     // 自分の得意な言語で
     // Let's チャレンジ！！
-<<<<<<< HEAD
     $inputLine = fgets(STDIN);
     $dayBefore = $inputLine - 1;
     echo $dayBefore;
-=======
-    $input_line = fgets(STDIN) -1;
-    echo "$input_line";
->>>>>>> parent of a35fd6c... refactor: 変数名をキャメルケースに変更
 ?>
