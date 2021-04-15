c648f07df61690a5010e176b0d2ea99aa3a98229
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-15 16:27:42 +0900

    refactor: 不要なコメントアウトを削除

--- C086.php
+++ C086.php
@@ -1,6 +1,4 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
     $inputLine = fgets(STDIN);
     $vowelRemove = strtr($inputLine, array('a' => '', 'e' => '', 'i' => '', 'o' => '', 'u' => '', 'A' => '', 'E' => '', 'I' => '', 'O' => '', 'U' => ''));
     echo $vowelRemove;
