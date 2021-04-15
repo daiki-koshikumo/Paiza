5b2c0fd63550991c3b45977daea8e5cfc3edc474
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-15 15:51:44 +0900

    refactor: 不要なコメントアウトを削除

--- C084.php
+++ C084.php
@@ -1,6 +1,4 @@
 <?php
-    // 自分の得意な言語で
-    // Let's チャレンジ！！
     $inputLine = fgets(STDIN);
     $inputLineAdjust = str_replace(PHP_EOL, '', $inputLine);
     $countInputLine = mb_strlen($inputLineAdjust);
