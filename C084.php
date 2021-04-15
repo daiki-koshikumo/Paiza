619c1aaa1c66e4c807334956231ee26ae07e25f9
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-15 15:49:25 +0900

    refactor: echoを文字列結合から変数展開へ変更

--- C084.php
+++ C084.php
@@ -5,5 +5,5 @@
     $inputLineAdjust = str_replace(PHP_EOL, '', $inputLine);
     $countInputLine = mb_strlen($inputLineAdjust);
     $decoration = str_repeat("+", $countInputLine + 2);
-    echo $decoration."\n"."+".$inputLineAdjust."+"."\n".$decoration;
+    echo "$decoration\n+$inputLineAdjust+\n$decoration";
 ?>
