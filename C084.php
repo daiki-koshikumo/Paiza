33b022c233afd852585b0f9f73e848a5ead45288
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-15 15:34:29 +0900

    refactor: 重複していた変数名を変更

--- C084.php
+++ C084.php
@@ -2,8 +2,8 @@
     // 自分の得意な言語で
     // Let's チャレンジ！！
     $inputLine = fgets(STDIN);
-    $inputLine = str_replace(PHP_EOL, '', $inputLine);
-    $countInputLine = mb_strlen($inputLine);
+    $inputLineAdjust = str_replace(PHP_EOL, '', $inputLine);
+    $countInputLine = mb_strlen($inputLineAdjust);
     $decoration = str_repeat("+", $countInputLine + 2);
-    echo $decoration."\n"."+".$inputLine."+"."\n".$decoration;
+    echo $decoration."\n"."+".$inputLineAdjust."+"."\n".$decoration;
 ?>
