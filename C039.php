7703345b2978436ce2350730a879dc454697d4ae
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-16 11:45:48 +0900

    refactor:不要な変数の文字列型への変換を削除

--- C039.php
+++ C039.php
@@ -3,5 +3,5 @@
     $firstPlace = substr_count($inputLine, "/");
     $tenthPlace = 10 * substr_count($inputLine, "<");
     $calculationResult = $firstPlace + $tenthPlace;
-    echo "$calculationResult";
+    echo $calculationResult;
 ?>
