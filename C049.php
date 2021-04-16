e79c6502117f262788eb4f9a89cb1fe4a0490c26
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-04-16 16:51:53 +0900

    add: C049.phpを追加

---
+++ C049.php
@@ -0,0 +1,22 @@
+<?php
+
+for ($inputLine = 0; $inputLine = trim(fgets(STDIN)); $inputLine++) {
+    $inputLineArray[] = $inputLine;
+}
+
+for($i = 0; $i < count($inputLineArray); $i++){
+    if($i === 0){
+      $elevatorMoved = 0;
+    }elseif($i === 1){
+      $goUpDown = $inputLineArray[1] - 1;
+    }else{
+      if($inputLineArray[$i] < $inputLineArray[$i-1]){
+        $goUpDown = $inputLineArray[$i-1] - $inputLineArray[$i];
+      }else{
+        $goUpDown= $inputLineArray[$i] - $inputLineArray[$i-1];
+      }
+    }
+    $elevatorMoved = $elevatorMoved + $goUpDown; 
+}
+echo $elevatorMoved;
+?>
