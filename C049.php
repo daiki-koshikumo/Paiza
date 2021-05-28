2ae61d2faddce12adc5407febe4f19a20931f420
daiki-koshikumo <daiki.koshikumo@asia-quest.jp>
2021-05-28 18:16:11 +0900

    refactor: classを使って書くように変更

--- C049.php
+++ C049.php
@@ -1,27 +1,32 @@
 <?php
-
-$stopedFloors = getInputLine();
-
-foreach($stopedFloors as $movedNumber => $inputLine){
-    $movedElevatorTotal += movedDifference($movedNumber, $stopedFloors, $movedElevatorTotal);
-}
-echo $movedElevatorTotal;
-
-function getInputLine(){
-    for($inputLine = 0; $inputLine = trim(fgets(STDIN)); $inputLine++){
-        $stopedFloors[] = $inputLine;
+class elevetor{
+    public function result(){
+        foreach($stopedFloors = $this->getInputLine() as $movedNumber => $movedTimes){
+        $movedElevatorTotal += $this->movedDifference($movedNumber, $stopedFloors, $movedElevatorTotal);
+        }
+    echo $movedElevatorTotal;
     }
-    //標準入力値の先頭は必要ないので除去する
-    array_shift($stopedFloors);
-    return $stopedFloors;
+    
+    public function getInputLine(){
+        for($movedTimes = 0; $movedTimes = trim(fgets(STDIN)); $movedTimes++){
+            $stopedFloors[] = $movedTimes;
+        }
+        array_shift($stopedFloors);
+        return $stopedFloors;
+    }
+    
+    public function movedFirstTime($movedElevatorTotal,$stopedFloors, $movedNumber){
+        $movedElevatorTotal = $stopedFloors[$movedNumber] - 1;
+        return $movedElevatorTotal;
+    }
+    
+    function movedDifference($movedNumber, $stopedFloors, $movedElevatorTotal){
+        $movedElevatorTotal = $movedNumber === 0? $this->movedFirstTime($movedElevatorTotal, $stopedFloors, $movedNumber)
+        : abs($stopedFloors[$movedNumber] - $stopedFloors[$movedNumber-1]);
+        return $movedElevatorTotal;
+    }
+    
 }
 
-function movedFirstTime($movedElevatorTotal,$stopedFloors, $movedNumber){
-    $movedElevatorTotal = $stopedFloors[$movedNumber] - 1;
-    return $movedElevatorTotal;
-}
-
-function movedDifference($movedNumber, $stopedFloors, $movedElevatorTotal){
-    $movedElevatorTotal = $movedNumber === 0? movedFirstTime($movedElevatorTotal, $stopedFloors, $movedNumber): abs($stopedFloors[$movedNumber] - $stopedFloors[$movedNumber-1]);
-    return $movedElevatorTotal;
-}
+$result = new elevetor;
+echo $result->result();
